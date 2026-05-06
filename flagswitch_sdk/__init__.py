3b91b0ececd55e00d51b7d719d5702656377a6e9
ayus79 <ayush@simprosys.com>
2026-05-06 20:01:03 +0530

    Add version and author metadata in __init__.py

--- flagswitch_sdk/__init__.py
+++ flagswitch_sdk/__init__.py
@@ -1,3 +1,6 @@
+__version__ = "0.2.0"
+__author__ = "FlagSwitch"
+
 from flagswitch_sdk.client import FlagSwitch, FlagSwitchSync
 from flagswitch_sdk.exceptions import (
     FlagSwitchConnectionError,
