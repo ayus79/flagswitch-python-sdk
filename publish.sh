753285dc74970b9058d0de45f5e77c4dd43632c7
ayus79 <ayush@simprosys.com>
2026-05-06 20:04:11 +0530

    Add publish script for building and uploading to PyPI

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PYPI_TOKEN="pypi-AgEIcHlwaS5vcmcCJDJkM2Y4N2Q3LTY4ZjUtNDk3OC04MGM0LWQwNDUxMjk5OTI2YwACKlszLCJkNjg5OWQ4NC0wMDAzLTQ4ZDQtOTY0ZC1kOGEwMjUwNGM3OGIiXQAABiAGkjK8MblnUD7W0ASWtaQqJJTvHhO0TlHgS0KZtDOLbw"
+
+echo "Building..."
+rm -rf dist
+python3 -m build
+
+echo "Uploading to PyPI..."
+python3 -m twine upload dist/* -u __token__ -p $PYPI_TOKEN
+
+echo "Done!"
